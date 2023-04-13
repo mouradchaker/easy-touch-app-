151377ad69777d8899297083f1c977c95a01c833
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-13 18:40:54 +0100

    userSlice

---
+++ frontend/src/app/userSlice.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userSlice = createApi({
+  reducerPath: "user",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://easytouch.onrender.com/auth",
+  }),
+  endpoints: (builder) => ({
+    loginUser: builder.mutation({
+      query: (data) => ({
+        url: "/login",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    registerUser: builder.mutation({
+      query: (data) => ({
+        url: "/signup",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginUserMutation, useRegisterUserMutation } = userSlice;
