312b1f1e1949d99ebd6ae9ee11d447aa5afdeddb
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-04-17 03:28:16 +0100

    header

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-center font-bold text-4xl pt-4 text-primary">
+      <a href="https://easytouchdz.web.app/">Easy Touch</a>
+    </div>
+  );
+};
+
+export default Header;
